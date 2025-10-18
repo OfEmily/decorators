6ec4c9ba4fda242db860114e6d0745a52cf7382b
unknown <emiliabronnik@yandex.ru>
2025-10-18 20:30:32 +0300

    Добавление эндпоинта

--- decorator_object.py
+++ decorator_object.py
@@ -2,21 +2,27 @@ class Flask:
     def __init__(self):
         self.routes = {}
     
-    def route(self, r):
+    def route(self, r, m):
         def wrapper(func):
-            self.routes[r] = func 
+            self.routes[r] = {m :  func} 
         return wrapper 
     
 app = Flask()
 
-@app.route("/products")
+@app.route("/products", "GET")
 def get_products():
     print("Hello")
 
+@app.route("/categories", "POST")
+def post_categories():
+    print("Bye")
+
 def main():
-    print(app.routes)
-    get_products = app.routes['/products']
+    get_products = app.routes['/products']["GET"]
     get_products()
+
+    post_categories = app.routes ['/categories']["POST"]
+    post_categories()
 
 if __name__=="__main__":
     main()
